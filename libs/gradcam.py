1744ee14d810c0724828791c2d80b735ffcef424
Sebastian Drost <s.drost@52north.org>
2022-05-03 11:16:55 +0200

    Simplify GradCAM creation

--- libs/gradcam.py
+++ libs/gradcam.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
 from PIL import Image
 
-
-def create_grad_cam_heatmap(model, inputs, pred_index, last_conv_layer, timestep):
+def create_grad_cam_heatmap(model, inputs, pred_index, last_conv_layer, scale_across_time):
     grad_model = tf.keras.models.Model(
         [model.inputs], [model.get_layer(last_conv_layer).output, model.output]
     )
@@ -23,30 +22,35 @@ def create_grad_cam_heatmap(model, inputs, pred_index, last_conv_layer, timestep
     for i in range(0, feature_maps.shape[0]):
         heatmap = feature_maps[i, ...] @ pooled_grads[i, ..., tf.newaxis]
         heatmap = tf.reshape(heatmap, (1, heatmap.shape[0], heatmap.shape[1]))
-        if tf.math.reduce_max(heatmap) == 0:
-            heatmap = tf.maximum(heatmap, 0)
-        else:
-            heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+
+        if not scale_across_time:
+            if tf.math.reduce_max(heatmap) == 0:
+                heatmap = tf.maximum(heatmap, 0)
+            else:
+                heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
 
         heatmap_res = tf.concat([heatmap_res, heatmap], axis=0)
 
-    # heatmap_res = tf.maximum(heatmap_res, 0) / tf.math.reduce_max(heatmap_res)
+    if scale_across_time:
+        heatmap_res = tf.maximum(heatmap_res, 0) / tf.math.reduce_max(heatmap_res)
 
     np_heatmap = heatmap_res.numpy()
 
-    # t = 25
-    image = inputs[0, timestep, ..., 0]
-    image_heatmap = np_heatmap[timestep, ...]
+    heatmap_list = []
 
-    image_heatmap = Image.fromarray(image_heatmap)
-    image_heatmap = image_heatmap.resize((image.shape[1], image.shape[0]))
-    image_heatmap = tf.keras.preprocessing.image.img_to_array(image_heatmap)
+    for t in range(0, np_heatmap.shape[0]):
+        image = inputs[0, t, ..., 0]
+        image_heatmap = np_heatmap[t, ...]
 
-    return image_heatmap
+        image_heatmap = Image.fromarray(image_heatmap)
+        image_heatmap = image_heatmap.resize((image.shape[1], image.shape[0]))
+        image_heatmap = tf.keras.preprocessing.image.img_to_array(image_heatmap)
+        heatmap_list.append(image_heatmap)
+
+    return heatmap_list
 
 
-def create_grad_lstm_cam_heatmap(model, inputs, pred_index, last_conv_layer, last_lstm_layer, timestep,
-                                 mixed_weighting, scale_across_time):
+def create_grad_lstm_cam_heatmap(model, inputs, pred_index, last_conv_layer, last_lstm_layer, mixed_weighting, scale_across_time):
 
     grad_model = tf.keras.models.Model(
         [model.inputs], [model.get_layer(last_conv_layer).output, model.output]
@@ -98,12 +102,15 @@ def create_grad_lstm_cam_heatmap(model, inputs, pred_index, last_conv_layer, las
 
     np_heatmap = heatmap_res.numpy()
 
-    # t = 25
-    image = inputs[0, timestep, ..., 0]
-    image_heatmap = np_heatmap[timestep, ...]
+    heatmap_list = []
 
-    image_heatmap = Image.fromarray(image_heatmap)
-    image_heatmap = image_heatmap.resize((image.shape[1], image.shape[0]))
-    image_heatmap = tf.keras.preprocessing.image.img_to_array(image_heatmap)
+    for t in range(0, np_heatmap.shape[0]):
+        image = inputs[0, t, ..., 0]
+        image_heatmap = np_heatmap[t, ...]
 
-    return image_heatmap
+        image_heatmap = Image.fromarray(image_heatmap)
+        image_heatmap = image_heatmap.resize((image.shape[1], image.shape[0]))
+        image_heatmap = tf.keras.preprocessing.image.img_to_array(image_heatmap)
+        heatmap_list.append(image_heatmap)
+
+    return heatmap_list
